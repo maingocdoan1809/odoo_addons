258b101107be675f14723a4c30f1fbe0675f951c
TSD - Mai Ngoc Doan <doanmn@nissho-vn.com>
2025-05-05 17:27:07 +0700

    Toggle navbar

--- backend_theme/static/src/components/app_menu/action_container.js
+++ backend_theme/static/src/components/app_menu/action_container.js
@@ -1,7 +1,33 @@
 
 import { NavSideBar } from "./search_apps";
 import { ActionContainer } from "@web/webclient/actions/action_container";
-import { xml } from "@odoo/owl";
+import { useEffect, useState, useSubEnv, xml } from "@odoo/owl";
+import { patch } from "@web/core/utils/patch";
+import { WebClient } from "@web/webclient/webclient";
+
+
+patch(WebClient.prototype, {
+    setup() {
+        super.setup()
+        this.navbarState = useState({
+            isOpen: false
+        })
+        useSubEnv({
+            navbarState: this.navbarState,
+            toggleNavBar: () => {
+                this.navbarState.isOpen = !this.navbarState.isOpen
+            }
+        })
+    }
+})
+
+
+patch(ActionContainer.prototype, {
+    setup() {
+        super.setup()
+        this.navbarState = useState(this.env.navbarState)
+    }
+})
 
 ActionContainer.components = {
     ...ActionContainer.components,
@@ -11,7 +37,7 @@ ActionContainer.components = {
 ActionContainer.template = xml`
 <t t-name="web.ActionContainer">
     <div class="o_action_manager">
-        <NavSideBar t-if="!env.isSmall"/>
+        <NavSideBar t-if="!env.isSmall and navbarState.isOpen"/>
         <t t-if="info.Component" t-component="info.Component" className="'o_action'" t-props="info.componentProps" t-key="info.id"/>
     </div>
 </t>`;
