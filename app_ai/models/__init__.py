5df39445e3a30634223edc6e60b513c6380d022f
TSD - Mai Ngoc Doan <doanmn@nissho-vn.com>
2025-05-22 16:41:38 +0700

    add ai addons

---
+++ app_ai/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of odooAi.cn. See LICENSE file for full copyright and licensing details.
+
+from . import ai_robot
+from . import res_config_settings
+from . import discuss_channel
+
